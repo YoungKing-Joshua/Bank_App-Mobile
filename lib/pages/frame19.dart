b943c0e41e277cbfcddca151e2856dbdba72ff42
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-09-04 15:40:10 +0000

    in progress

--- lib/pages/frame19.dart
+++ lib/pages/frame19.dart
@@ -11,7 +11,6 @@ class _Bottomsheet extends State<Bottomsheet> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      
       child: ElevatedButton(
         child: const Text('Model Bottom Sheet'),
         onPressed: () {
@@ -35,7 +34,7 @@ class _Bottomsheet extends State<Bottomsheet> {
                 topRight: Radius.circular(32),
               ),
             ),
-            child: Stack(
+            child: Column(
               children: [
                 Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                   Padding(
@@ -50,6 +49,178 @@ class _Bottomsheet extends State<Bottomsheet> {
                     ),
                   ),
                 ]),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Transaction Details',
+                      style: TextStyle(
+                        color: Color(0xFF001530),
+                        fontSize: 14,
+                        fontFamily: 'Open Sans',
+                        fontWeight: FontWeight.w400,
+                        height: 1.20,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Balance Before Transaction',
+                            style: TextStyle(
+                              color: Color(0xFF212121),
+                              fontSize: 12,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w400,
+                              height: 1.20,
+                            ),
+                          ),
+                          Text(
+                            'GHC 0.00',
+                            style: TextStyle(
+                              color: Color(0xFF212121),
+                              fontSize: 12,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w700,
+                              height: 1.20,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Balance After Transaction',
+                            style: TextStyle(
+                              color: Color(0xFF212121),
+                              fontSize: 12,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w400,
+                              height: 1.20,
+                            ),
+                          ),
+                          Text(
+                            'GHC 10,000.00',
+                            style: TextStyle(
+                              color: Color(0xFF212121),
+                              fontSize: 12,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w700,
+                              height: 1.20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color(0x99E1E6F0),
+                      width: .75,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Transaction Date',
+                            style: TextStyle(
+                              color: Color(0xFF212121),
+                              fontSize: 12,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w400,
+                              height: 1.20,
+                            ),
+                          ),
+                          Text(
+                            '01-02-2021',
+                            style: TextStyle(
+                              color: Color(0xFF212121),
+                              fontSize: 12,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w600,
+                              height: 1.20,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Transaction Direction',
+                            style: TextStyle(
+                              color: Color(0xFF212121),
+                              fontSize: 12,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w400,
+                              height: 1.20,
+                            ),
+                          ),
+                          Text(
+                            'Credit',
+                            style: TextStyle(
+                              color: Color(0xFF212121),
+                              fontSize: 12,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w600,
+                              height: 1.20,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Transaction Narration',
+                            style: TextStyle(
+                              color: Color(0xFF212121),
+                              fontSize: 12,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w400,
+                              height: 1.20,
+                            ),
+                          ),
+                          Text(
+                            'Deposit',
+                            style: TextStyle(
+                              color: Color(0xFF212121),
+                              fontSize: 12,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w600,
+                              height: 1.20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ));
